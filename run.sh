27ad9e90943e6bb74ee3cce3750de3b9ab372aae
Choong Jun Jin <choong.junjin@gmail.com>
2017-05-12 19:23:10 +0900

    modified run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 #-v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
 
 nvidia-docker run -ti --rm \
-    -e LOCAL_USER_I=`id -u $USER` \
-    -v /home/choong/Projects:/home/choong/work \
+    -e LOCAL_USER=`id -u $USER` \
+    -v /home/$USER:/home/$USER/mnt \
     -v /mnt/data/:/mnt/data \
-    -v caffe_data:/opt/caffe/models \
+    -v caffe_models:/opt/caffe/models \
+    -v caffe_examples:/opt/caffe/examples \
+    -v caffe_data:/opt/caffe/data \
     --network host \
     caffe:jupyter bash
